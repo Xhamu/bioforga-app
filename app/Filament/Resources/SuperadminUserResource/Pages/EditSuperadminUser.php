83e1b8f0bcc4e62aac6cd825f2b8dbe3c2fdd92f
Samuel Rodriguez <srodriguez@quadralia.com>
2025-06-09 09:41:43 +0200

    Actualizacón para manejar bien las passwords de usuarios.

--- app/Filament/Resources/SuperadminUserResource/Pages/EditSuperadminUser.php
+++ app/Filament/Resources/SuperadminUserResource/Pages/EditSuperadminUser.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\SuperadminUserResource\Pages;
 
 use App\Filament\Resources\SuperadminUserResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use STS\FilamentImpersonate\Impersonate;
 
 class EditSuperadminUser extends EditRecord
 {
@@ -15,5 +17,35 @@ class EditSuperadminUser extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function getActions(): array
+    {
+        return [
+            Impersonate::make()->record($this->getRecord()) // <--
+        ];
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Usuario actualizado');
+    }
+
+    protected function afterSave()
+    {
+        return redirect('/superadmin-users');
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (empty($data['password'])) {
+            unset($data['password']);
+        } else {
+            $data['password'] = bcrypt($data['password']);
+        }
+
+        return $data;
     }
 }
